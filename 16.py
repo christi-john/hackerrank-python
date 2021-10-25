218773bc3d05fe63a9ef9c7b760ec815f51c4a4f
Christiana <christiana.john1@gmail.com>
2021-10-25 23:27:10 +0530

    Problem 16

---
+++ 16.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n1=int(input())
+n2=int(input())
+ans=divmod(n1,n2)
+print(ans[0])
+print(ans[1])
+print(ans)
