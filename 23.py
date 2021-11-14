a61cad586903efbf31badcf60d51ed12c7a4551f
Christiana <christiana.john1@gmail.com>
2021-11-14 10:46:05 +0530

    Problem 23

---
+++ 23.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/itertools-product/problem
+
+from itertools import product
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+print(*list(product(A,B)))
